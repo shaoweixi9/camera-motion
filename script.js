88a81fe6fa600094ce7e2e9a0bcc21fe7770fd05
shaoweixi9 <shaoweixi9@gmail.com>
2026-01-10 23:43:34 +0800

    改成了腾讯云图床

--- script.js
+++ script.js
@@ -2,51 +2,8 @@
 let cameraMovementsData = [];
 let currentMovement = null;
 
-// Cloudinary视频URL映射表 - 与本地文件名一一对应
-const videoUrlMap = {
-    '01.mp4': 'https://res.cloudinary.com/drcq5lsmz/video/upload/v1768056519/01_mclvyq.mp4',
-    '02.mp4': 'https://res.cloudinary.com/drcq5lsmz/video/upload/v1768056512/02_bd1yqq.mp4',
-    '03.mp4': 'https://res.cloudinary.com/drcq5lsmz/video/upload/v1768056513/03_i1ifeq.mp4',
-    '04.mp4': 'https://res.cloudinary.com/drcq5lsmz/video/upload/v1768056511/04_acdysb.mp4',
-    '05.mp4': 'https://res.cloudinary.com/drcq5lsmz/video/upload/v1768056515/05_jqc8vd.mp4',
-    '06.mp4': 'https://res.cloudinary.com/drcq5lsmz/video/upload/v1768056514/06_odlxvm.mp4',
-    '07.mp4': 'https://res.cloudinary.com/drcq5lsmz/video/upload/v1768056528/07_ebkils.mp4',
-    '08.mp4': 'https://res.cloudinary.com/drcq5lsmz/video/upload/v1768056536/08_z326rl.mp4',
-    '09.mp4': 'https://res.cloudinary.com/drcq5lsmz/video/upload/v1768056529/09_nrgjs4.mp4',
-    '10.mp4': 'https://res.cloudinary.com/drcq5lsmz/video/upload/v1768056512/10_wfd1xv.mp4',
-    '11.mp4': 'https://res.cloudinary.com/drcq5lsmz/video/upload/v1768056511/11_tusmff.mp4',
-    '12.mp4': 'https://res.cloudinary.com/drcq5lsmz/video/upload/v1768056511/12_stigyv.mp4',
-    '13.mp4': 'https://res.cloudinary.com/drcq5lsmz/video/upload/v1768056512/13_odj1ag.mp4',
-    '14.mp4': 'https://res.cloudinary.com/drcq5lsmz/video/upload/v1768056514/14_js8sp1.mp4',
-    '15.mp4': 'https://res.cloudinary.com/drcq5lsmz/video/upload/v1768056514/15_py682u.mp4',
-    '16.mp4': 'https://res.cloudinary.com/drcq5lsmz/video/upload/v1768056525/16_rmk3d1.mp4',
-    '17.mp4': 'https://res.cloudinary.com/drcq5lsmz/video/upload/v1768056515/17_n30inf.mp4',
-    '18.mp4': 'https://res.cloudinary.com/drcq5lsmz/video/upload/v1768056516/18_woypb0.mp4',
-    '19.mp4': 'https://res.cloudinary.com/drcq5lsmz/video/upload/v1768056516/19_pmo6ho.mp4',
-    '20.mp4': 'https://res.cloudinary.com/drcq5lsmz/video/upload/v1768056527/20_gobhsw.mp4',
-    '21.mp4': 'https://res.cloudinary.com/drcq5lsmz/video/upload/v1768056516/21_m02nkk.mp4',
-    '22.mp4': 'https://res.cloudinary.com/drcq5lsmz/video/upload/v1768056519/22_m8i58q.mp4',
-    '23.mp4': 'https://res.cloudinary.com/drcq5lsmz/video/upload/v1768056518/23_ybfwg9.mp4',
-    '24.mp4': 'https://res.cloudinary.com/drcq5lsmz/video/upload/v1768056518/24_bypinq.mp4',
-    '25.mp4': 'https://res.cloudinary.com/drcq5lsmz/video/upload/v1768056519/25_qog0te.mp4',
-    '26.mp4': 'https://res.cloudinary.com/drcq5lsmz/video/upload/v1768056520/26_fkh4ep.mp4',
-    '27.mp4': 'https://res.cloudinary.com/drcq5lsmz/video/upload/v1768056521/27_cbfzwx.mp4',
-    '28.mp4': 'https://res.cloudinary.com/drcq5lsmz/video/upload/v1768056541/28_ssguln.mp4',
-    '29.mp4': 'https://res.cloudinary.com/drcq5lsmz/video/upload/v1768056521/29_l0qhiu.mp4',
-    '30.mp4': 'https://res.cloudinary.com/drcq5lsmz/video/upload/v1768056522/30_tzipca.mp4',
-    '31.mp4': 'https://res.cloudinary.com/drcq5lsmz/video/upload/v1768056522/31_n9beql.mp4',
-    '32.mp4': 'https://res.cloudinary.com/drcq5lsmz/video/upload/v1768056523/32_luesbq.mp4',
-    '33.mp4': 'https://res.cloudinary.com/drcq5lsmz/video/upload/v1768056524/33_wpofp9.mp4',
-    '34.mp4': 'https://res.cloudinary.com/drcq5lsmz/video/upload/v1768056524/34_mu7ykt.mp4',
-    '35.mp4': 'https://res.cloudinary.com/drcq5lsmz/video/upload/v1768056525/35_gkxscc.mp4',
-    '36.mp4': 'https://res.cloudinary.com/drcq5lsmz/video/upload/v1768056525/36_f0dx5h.mp4',
-    '37.mp4': 'https://res.cloudinary.com/drcq5lsmz/video/upload/v1768056525/37_hgjlz0.mp4',
-    '38.mp4': 'https://res.cloudinary.com/drcq5lsmz/video/upload/v1768056526/38_ui5p3e.mp4',
-    '39.mp4': 'https://res.cloudinary.com/drcq5lsmz/video/upload/v1768056526/39_n7aaph.mp4',
-    '40.mp4': 'https://res.cloudinary.com/drcq5lsmz/video/upload/v1768056535/40_mkatts.mp4',
-    '41.mp4': 'https://res.cloudinary.com/drcq5lsmz/video/upload/v1768056527/41_irki6k.mp4',
-    '42.mp4': 'https://res.cloudinary.com/drcq5lsmz/video/upload/v1768056527/42_tmlugw.mp4'
-};
+// 腾讯云COS视频URL前缀
+const TENCENT_CLOUD_PREFIX = 'https://cam-motion-1394308214.cos.ap-beijing.myqcloud.com/Cam-Motion-shots/';
 
 // 页面加载完成后执行
 document.addEventListener('DOMContentLoaded', () => {
@@ -141,9 +98,9 @@ function updateVideoPreview(movement) {
     const videoElement = document.getElementById('previewVideo');
     const videoFileName = movement['文件名'];
 
-    if (videoFileName && videoUrlMap[videoFileName]) {
-        // 使用Cloudinary视频URL映射表中的URL
-        const cloudVideoUrl = videoUrlMap[videoFileName];
+    if (videoFileName) {
+        // 使用腾讯云COS统一地址前缀 + 文件名
+        const cloudVideoUrl = `${TENCENT_CLOUD_PREFIX}${encodeURIComponent(videoFileName)}`;
         videoElement.src = cloudVideoUrl;
         videoElement.load();
     } else {
